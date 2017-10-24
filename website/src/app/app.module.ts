4d0dcedc51f25115639ca8c364babcbddeabccd3
AuslinPJ <31402534+AuslinPJ@users.noreply.github.com>
2017-10-24 12:40:25 +0530

    footer design

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MatToolbarModule,MatIconModule} from '@angular/material';
+
+//NgModule for material 
+@NgModule({
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+  ]
+})
+export class MaterialModule { }
+//material done
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
