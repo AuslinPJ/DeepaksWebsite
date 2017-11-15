bc22021d92aae59e1d8eae01bd4f4e0f90c7661a
Pradhisha <pradhisha@gmail.com>
2017-11-15 14:55:02 +1100

    edit testimonial

--- website/src/app/testimonial/testimonial.component.ts
+++ website/src/app/testimonial/testimonial.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-//import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
-import { AngularFireDatabase, FirebaseListObservable } from "angularfire2/database-deprecated";
-import { AngularFireAuth } from 'angularfire2/auth';
-import { Observable } from 'rxjs/Observable';
-import * as firebase from 'firebase/app';
-import { AngularFireModule } from 'angularfire2';
+
+
+
 
 @Component({
   selector: 'app-testimonial',
@@ -12,33 +9,5 @@ import { AngularFireModule } from 'angularfire2';
   styleUrls: ['./testimonial.component.css']
 })
 export class TestimonialComponent   {
-
-  user: Observable<firebase.User>;
-  items: FirebaseListObservable<any[]>;
-  msgVal: string = '';
-
-  constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
-    this.items = af.list('/messages', {
-      query: {
-        limitToLast: 50
-      }
-    });
-
-    this.user = this.afAuth.authState;
-
-  }
-
-  login() {
-    this.afAuth.auth.signInAnonymously();
-}
-
-logout() {
-    this.afAuth.auth.signOut();
-}
-
-Send(desc: string) {
-    this.items.push({ message: desc});
-    this.msgVal = '';
-}
-
+constructor() {}
 }
